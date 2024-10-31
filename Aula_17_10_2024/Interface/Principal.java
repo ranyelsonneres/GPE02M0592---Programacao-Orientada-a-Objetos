d10ff4cb55196553cee08e356e7ae088f6f6b788
Ranyelson Neres <ranyelson.carvalho@p.ucb.br>
2024-10-31 09:08:46 -0300

    Update Principal.java

--- Aula_17_10_2024/Interface/Principal.java
+++ Aula_17_10_2024/Interface/Principal.java
@@ -1,1 +1,22 @@
 //implementar a classe principal.
+
+package interfaces;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		RadioRelogio radio = new RadioRelogio();
+		
+		radio.ligar();
+		System.out.println("Estação atual: " + radio.obterEstacaoAtual());
+		
+		radio.trocarEstacao(110.5);
+		System.out.println("Estação atual: " + radio.obterEstacaoAtual());
+		System.out.println("Hora: " + radio.obterHoras());
+		
+		radio.desligar();
+		
+	}
+
+}

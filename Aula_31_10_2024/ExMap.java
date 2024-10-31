afd79ba56d2cded7dc630d9b07d5b3ca4c6df2a5
Ranyelson Neres <ranyelson.carvalho@p.ucb.br>
2024-10-31 11:19:45 -0300

    Create ExMap.java

---
+++ Aula_31_10_2024/ExMap.java
@@ -0,0 +1,22 @@
+package colecoes;
+
+import java.util.*; 
+
+public class ExMap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//par: chave-valor
+		
+		Map<Integer, String> nomes = new HashMap<>();
+		
+		//adicionar
+		nomes.put(1, "Bob");
+		nomes.put(2, "Pedro");
+		nomes.put(3, "Pedro");
+		
+		System.out.println(nomes);
+		
+	}
+
+}
